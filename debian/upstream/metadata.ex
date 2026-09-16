ae24643b79d75eee8453d637fb9b3a3612b3eca3
coolshou <jimmy_yeh@alphanetworks.com>
2026-09-16 13:38:38 +0800

    add debian files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/qgemini/issues
+# Bug-Submit: https://github.com/<user>/qgemini/issues/new
+# Changelog: https://github.com/<user>/qgemini/blob/master/CHANGES
+# Documentation: https://github.com/<user>/qgemini/wiki
+# Repository-Browse: https://github.com/<user>/qgemini
+# Repository: https://github.com/<user>/qgemini.git
